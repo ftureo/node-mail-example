e2f06a037fb91ddad9066c56651e4297796e22f7
fabiantureo <fabianestebantureo@gmail.com>
2022-06-03 18:36:09 +0000

    Servr runnning and initial config ok

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const PORT = 4000
+
+app.use(express.urlencoded({ extended: false }))
+app.use(require('./routes/index'))
+app.use(express.static(path.join(__dirname, "public")))
+
+app.listen(PORT, () =>{
+    console.log(`App is running in port ${PORT}`)
+})
