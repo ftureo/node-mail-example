90779506c391a63644e04c1469823af13502775d
fabiantureo <fabianestebantureo@gmail.com>
2022-06-03 19:04:44 +0000

    🐛 fix: fixed root folder, script for development and preview for nodemailer

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ const path = require('path')
 const PORT = 4000
 
 app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
 app.use(require('./routes/index'))
-app.use(express.static(path.join(__dirname, "public")))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.listen(PORT, () =>{
     console.log(`App is running in port ${PORT}`)
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,20 @@
 const { Router } = require('express')
 const router = Router()
 
-router.post('send-email', (req, res) => {
+router.post('/send-email', (req, res) => {
     console.log("Body", req.body)
+    const { name, email, phone, message } = req.body
+
+    contentHTML=`
+    <h1>User Information</h1>
+    <ul>
+        <li>Name: ${name}</li>
+        <li>email: ${email}</li>
+        <li>phone: ${phone}</li>
+    </ul>
+    <p>${message}</p>
+    `
+    console.log(contentHTML)
     res.send("received")
 })
 
